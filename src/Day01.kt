5310e960157334fb054e8be4b82a38a5d94c32f1
Laurentiu <Laurentiu@DESKTOP-DU0RLR6>
2021-12-01 12:23:43 +0200

    First day problems

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    part1(input)
+    part2(input)
+}
+
+fun part1(input: List<String>): Int {
+    var result = 0
+    input.forEachIndexed { index, string ->
+        if (index < input.size - 1) {
+            if (input[index].toInt() < input[index + 1].toInt()) {
+                result += 1
+            }
+        }
+    }
+    println(result)
+    return result
+}
+
+fun part2(input: List<String>): Int {
+    var result = 0
+    var sum: Int
+    input.forEachIndexed { index, string ->
+        if (index < input.size - 3) {
+            sum = sumOfThreeNumbersFromIndex(index, input)
+            if ( sum < sumOfThreeNumbersFromIndex(index+1, input)) {
+                result += 1
+            }
+        }
+    }
+    println(result)
+    return result
+}
+
+fun sumOfThreeNumbersFromIndex(index: Int, input: List<String>) : Int {
+    var sum = 0
+    input.subList(index, index+3).forEach { number->
+        sum += number.toInt()
+    }
+    return sum
 }
